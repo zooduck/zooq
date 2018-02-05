6feba89e87e7839a4376bdfb28cccb075fb6c989
ZOODUCK <zooduck@beniceoreatsomerice.com>
2018-02-05 08:06:15 +0000

    refactor api/services/update.all.js to use mongodb

--- api/services/update.all.js
+++ api/services/update.all.js
@@ -3,66 +3,69 @@ const fs = require("fs");
 // methods...
 const servicesUpdateAll = (function servicesUpdateAll () {
 	const $run = (payload) => {
-		const companyIdAsKey = `_${payload.params.companyId}`;
-		const db = "./db/services.db.json";
-		let payloadServices = JSON.parse(payload.data);
-		let services = {};
-
-		for (const service of payloadServices) {
-			delete service._links;
-		}
-		payloadServices = payloadServices.filter( (service) => !service.queuing_disabled);
-
-		// ====================================================
-		// TEST UPDATE SERVICES COLLECTION IN MONGODB (MLAB)
-		// ====================================================
-		payload.dbo.collection("services").insertMany(payloadServices, (err, result) => {
-			if (err) return console.log(err);
-			console.log(`inserted ${result.insertedCount} docs into services`);
-		});
-
-		// ====================================================================================================================
-		// TODO: add services that are not there (but exist in payload), delete services which are there (but not in payload)
-		// ====================================================================================================================
-		payload.dbo.collection("services").find({}).toArray( (err, dbServices) => {
-			if (err) return console.log(err);
-			for (const service of dbServices) { // loop all docs...
-				// payload service exists in database?
-				const serviceExistsInDatabase = payloadServices.find( (item) => item.id == service.id);
-				if (!serviceExistsInDatabase) {
-					// TODO: add the service to the database
-				}
-				// database service exists in payload?
-				const serviceExistsInPayload = payload.dbo.collection("services").find(service).limit(1);
-				if (!serviceExistsInPayload) {
-					// TODO: delete the service from database
-				}
+		return new Promise( (resolve, reject) => {
+			const companyIdAsKey = `_${payload.params.companyId}`;
+			const db = "./db/services.db.json";
+			let payloadServices = JSON.parse(payload.data);
+			let services = {};
+			for (const service of payloadServices) {
+				delete service._links;
 			}
-		});
-
-		return new Promise((resolve, reject) => {
-			// read db...
-			fs.readFile(db, "utf8", (err, data) => {
+			payloadServices = payloadServices.filter( (service) => !service.queuing_disabled);
+			// ====================================================================================================================
+			// add services that are not there (but exist in payload), delete services which are there (but not in payload)
+			// ====================================================================================================================
+			payload.dbo.collection("services").find({}).toArray( (err, dbServices) => {
 				if (err) {
 					console.log(err);
-					reject(err);
+					return reject(err);
 				}
-				if (data && data != "") {
-					services = JSON.parse(data);
+				for (const dbService of dbServices) { // loop all docs...
+					// payload service exists in database?
+					const serviceToUpdate = payloadServices.find( (item) => item.id == dbService.id);
+					// DA
+					if (serviceToUpdate) {
+						// update service in database with latest data from bookingbug api (in payloadServices)
+						// -------------------------------------------------------------------------------------------------------------
+						// NOTE: this will not modify (overwrite) the document in the collection if it contains exactly the same data
+						// -------------------------------------------------------------------------------------------------------------
+						const q = { id: serviceToUpdate.id };
+						payload.dbo.collection("services").updateOne(q, {$set: serviceToUpdate}, (err, result) => {
+							if (err) {
+								console.log(err);
+								return reject(err);
+							}
+							console.log(`UPDATED => ${result.modifiedCount} docs in services`);
+						});
+					} else {
+						// service no longer exists, delete it!
+						payload.dbo.collection("services").deleteOne(dbService, (err, result) => {
+							if (err) {
+								console.log(err);
+								return reject(err);
+							}
+							console.log(`DELETED => ${result.deletedCount} docs from services`);
+						});
+					}
 				}
-				let alphabet = new String("abcdefghijklmnopqrstuvwxyz").split("");
-				for (service of payloadServices) {
-					service.code = alphabet.shift().toUpperCase();
+				for (const payloadService of payloadServices) {
+					const serviceExists = dbServices.find( (item) => item.id == payloadService.id);
+					if (!serviceExists) {
+						// service is new, add it!
+						payload.dbo.collection("services").insertOne(payloadService, (err, result) => {
+							console.log(err);
+							return reject(err);
+						});
+						console.log(`INSERTED => ${result.insertedCount} docs into services`);
+					}
 				}
-				services[companyIdAsKey] = payloadServices;
-				// write db...
-				fs.writeFile(db, JSON.stringify(services), "utf8", (err) => {
+				payload.dbo.collection("services").find({}).toArray( (err, result) => {
 					if (err) {
 						console.log(err);
-						reject(err);
-					} else {
-						resolve(JSON.stringify(services)); // all services for all child companies
+						return reject(err);
 					}
+					services[companyIdAsKey] = result;	
+					return resolve(JSON.stringify(services));
 				});
 			});
 		});
