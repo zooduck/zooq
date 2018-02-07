d980b65469330a7303cbfeacc972bd0b842c6649
zooduck <zooduck@beniceoreatsomerice.com>
2018-02-07 21:41:16 +0000

    migrate api/staff/attendance/1_available/update.js to mongodb

--- api/staff/attendance/1_available/update.js
+++ api/staff/attendance/1_available/update.js
@@ -1,60 +1,64 @@
 // dependencies...
-const fs = require("fs");
 const luxon = require("luxon");
 // methods...
 const staffUpdateOne = (function staffUpdateOne() {
   const $run = (payload) => {
     const companyIdAsKey = `_${payload.params.companyId}`;
     const actionType = payload.params.actionType;
-    const payloadStaffId = payload.id;
-    const staffDb = "./db/staff.db.json";
+    const payloadStaffMemberId = parseInt(payload.id);
 
     return new Promise( (resolve, reject) => {
-      // =============================
-      // STAFF MEMBER START SHIFT
-      // =============================
-      fs.readFile(staffDb, "utf8", (err, data) => {
+      // ===========================
+      // ATTENDANCE_STATUS LEGEND
+      // ===========================
+      // 0: AWOL (AWAY)
+      // 1: AVAILABLE
+      // 2: ON BREAK
+      // 3: BUSY (OTHER)
+      // 4: BUSY (IN APPOINTMENT)
+      // ===========================
+      const data = {
+        attendance_status: 1,
+        appointmentTimeLeft: 0,
+        attendance_ended: null,
+        activeBooking: null,
+        activeBookingType: null,
+        activeBreak: null,
+        activeBusy: null
+      }
+      if (actionType == "START_SHIFT") {
+        // ============================
+        // init for start shift ONLY
+        // ============================
+        data.attendance_started = luxon.DateTime.local().toISO();
+      }
+      payload.dbo.collection("staff").update({id: payloadStaffMemberId}, {$set: data}, (err, result) => {
+        if (err) {
+          console.log(err);
+          return reject(err);
+        }
+        payload.dbo.collection("staff").find({id: payloadStaffMemberId}).toArray( (err, result) => {
           if (err) {
             console.log(err);
-            reject(JSON.stringify({error: err}));
+            return reject(err);
           }
-          if (data && data != "") {
-            let staffCollections = JSON.parse(data);
-            let staff = staffCollections[companyIdAsKey];
-            let staffMember = staff.find( (item) => item.id == payloadStaffId);
-            // ===========================
-            // ATTENDANCE_STATUS LEGEND
-            // ===========================
-            // 0: AWOL (AWAY)
-            // 1: AVAILABLE
-            // 2: ON BREAK
-            // 3: BUSY (OTHER)
-            // 4: BUSY (IN APPOINTMENT)
-            // ===========================
-            staffMember.attendance_status = 1;
-            if (actionType == "START_SHIFT") {
-              // ============================
-              // init for start shift ONLY
-              // ============================
-              staffMember.attendance_started = luxon.DateTime.local().toISO();
-            }
-            staffMember.appointmentTimeLeft = 0;
-            staffMember.attendance_ended = null;
-            staffMember.activeBooking = null;
-            staffMember.activeBookingType = null;
-            staffMember.activeBreak = null;
-            staffMember.activeBusy = null;
-            fs.writeFile(staffDb, JSON.stringify(staffCollections), "utf8", (err) => {
-              if (err) {
-                console.log(err);
-                reject(JSON.stringify({error: err}));
-              }
-              // resolve(JSON.stringify(staffCollections)); // all staff for all companies
-              resolve(JSON.stringify(staffMember)); // the updated staff member
-            });
-          } else reject(JSON.stringify({error: "NO_DATA"}));
+          const Pusher = require('pusher');
+          const pusher = new Pusher({
+            appId: "451830",
+            key: "991a027aa0c940510776",
+            secret: "e1e453012d89603adc67",
+            cluster: "eu",
+            encrypted: true
+          });
+          // push message to client...
+          pusher.trigger("queue-channel", "queue-event", {
+            "message": "staff.db.json: changed",
+            "type": "staff.db.json"
+          });
+          return resolve(JSON.stringify(result));
+        });
       });
-    });
+    }); // end Promise
   }
   return function () {
     return {
