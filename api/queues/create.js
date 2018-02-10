f93fd2e143119a83fe0a3f6c04beda537b5a57bd
zooduck <zooduck@beniceoreatsomerice.com>
2018-02-10 15:51:22 +0000

    move method for generating queue codes to api and add createdAt prop to queue. apply sort modifier to mongodb queue requests.

--- api/queues/create.js
+++ api/queues/create.js
@@ -1,49 +1,71 @@
 // dependencies...
 const _ = require("lodash");
+const luxon = require("luxon");
 // methods...
 const queuesCreateOne = (function queuesCreateOne () {
 	const $run = (payload) => {
 		const companyIdAsKey = `_${payload.params.companyId}`;
     let payloadQueue = JSON.parse(payload.data);
     let oldQueues = {}
-    payload.dbo.collection("q").find({}).toArray( (err, result) => {
-      if (err) {
-        console.log(err);
-        return reject(err);
-      }
-      oldQueues[companyIdAsKey] = result;
-    });
+
     return new Promise( (resolve, reject) => {
-      payload.dbo.collection("q").insertOne(payloadQueue, (err, result) => {
-        if (err) {
-          console.log(err);
-          return reject(err);
-        }
-        payload.dbo.collection("q").find({}).toArray( (err, result) => {
-          if (err) {
-            console.log(err);
-            return reject(err);
-          }
-          const queues = {}
-          queues[companyIdAsKey] = result;
-          if (!_.isEqual(queues, oldQueues)) {
-            const Pusher = require('pusher');
-            const pusher = new Pusher({
-              appId: "451830",
-              key: "991a027aa0c940510776",
-              secret: "e1e453012d89603adc67",
-              cluster: "eu",
-              encrypted: true
-            });
-            // push message to client...
-            pusher.trigger("queue-channel", "queue-event", {
-              "message": "q.db.json: changed",
-              "type": "q.db.json"
-            });
-          }
-          return resolve(JSON.stringify(queues));
-        });
-      })
+			payload.dbo.collection("q").find({}).toArray( (err, result) => {
+				if (err) {
+					console.log(err);
+					return reject(err);
+				}
+				oldQueues[companyIdAsKey] = result;
+				// ==================
+				// set queue.code
+				// ==================
+				const totalQueues = result.length;
+				const codeKeys = Array.from("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+				payloadQueue.code = codeKeys[totalQueues] + codeKeys.reverse()[totalQueues];
+				// ======================
+				//  set queue.createdAt
+				// ======================
+				payloadQueue.createdAt = luxon.DateTime.local().toISO();
+				// ====================
+				//  INSERT QUEUE
+				// ====================
+				payload.dbo.collection("q").insertOne(payloadQueue, (err, result) => {
+					if (err) {
+						console.log(err);
+						return reject(err);
+					}
+					payload.dbo.collection("q").find({}).sort({createdAt: 1}).toArray( (err, result) => {
+						if (err) {
+							console.log(err);
+							return reject(err);
+						}
+						const queues = {}
+						queues[companyIdAsKey] = result;						
+						if (!_.isEqual(queues, oldQueues)) {
+							const Pusher = require('pusher');
+							const pusher = new Pusher({
+								appId: "451830",
+								key: "991a027aa0c940510776",
+								secret: "e1e453012d89603adc67",
+								cluster: "eu",
+								encrypted: true
+							});
+							// push message to client...
+							pusher.trigger("queue-channel", "queue-event", {
+								"message": "q.db.json: changed",
+								"type": "q.db.json"
+							});
+						}
+						return resolve(JSON.stringify(queues));
+					});
+				})
+			});
+
+
+
+
+
+
+
     }); // end Promise
 	}
 	return function () {
