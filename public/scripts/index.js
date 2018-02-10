9dd91c3f73d61249542d64e32d97ecc87908378e
zooduck <zooduck@beniceoreatsomerice.com>
2018-02-10 17:52:19 +0000

    modularise customer create form submit ctrl

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -118,27 +118,11 @@ function setEventListenersForStaticContent () {
 		queueCreateForm__submitCtrl__EVENT();
 	});
 
+	// ========================
 	// CTRL: CREATE CUSTOMER
+	// ========================
 	zooqueue.elements("customerCreateForm__submitCtrl").addEventListener("click", function (e) {
-		if (!zooqueue.hasQueues()) {
-			zooqueue.alert("QUEUE_NOT_FOUND");
-			return zooqueue.consoleError("QUEUE_NOT_FOUND");
-		}
-		const formData = new FormData(zooqueue.elements("customerCreateForm").querySelector("form"));
-		const data = zooqueueApi().convertCustomerFormDataToJson(formData);
-
-		if (data.error) {
-			return zooqueue.consoleError(data.error);
-		}
-
-		zooqueueApi().customerCreate(data).then( (result) => {
-			clearForm(zooqueue.elements("customerCreateForm").querySelector("form"));
-			zooqueue.consoleLog(result);
-			zooqueue.removeFilters(["customer"]);
-			// buildDom(); // use pusher instead
-		}, err => {
-			zooqueue.consoleError(err);
-		});
+		customerCreateForm__submitCtrl__EVENT();
 	});
 
 	// ============================
