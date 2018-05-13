bdf6fa59b7bd8617ff792eadbfda489068fffdb1
ZOODUCK <zooduck@beniceoreatsomerice.com>
2018-05-13 12:32:58 +0100

    Add console logs showing staff/service bindings to current queue.

--- public/scripts/dom/build/buildStaffCards.js
+++ public/scripts/dom/build/buildStaffCards.js
@@ -12,11 +12,29 @@ function buildStaffCards(filters) {
     // =============================================================================
     // NOTE: STAFF MEMBER MUST SUPPORT AT LEAST ONE SERVICE IN THE CURRENT QUEUE
     // =============================================================================
+
+    // == log =================================================
+    zooq.consoleLog({
+      name: staffMember.name,
+      hasServices: zooq.staffMemberHasServices(staffMember),
+      queuing_disabled: staffMember.queuing_disabled
+    });
+    // == \log ================================================
+
     if (zooq.staffMemberHasServices(staffMember) && !staffMember.queuing_disabled) {
       staffMember.attendance_status == 0? staffAway.push(staffMember) : staffMember.attendance_status == 1? staffFree.push(staffMember) : staffBusy.push(staffMember);
       // staffMember.attendance_started && staffMember.attendance_status != 1? staffBusy.push(staffMember) : staffFree.push(staffMember);
     }
   }
+
+  // == log =========================================================================
+  zooq.consoleLog(staff.map( (staffMember) => {
+    if (zooq.staffMemberHasServices(staffMember) && !staffMember.queuing_disabled) {
+      return staffMember.name;
+    }
+  }).join(', ').replace(/,\s(?![^,])+/g, ""), "have services in this queue");
+  // == \log ========================================================================
+
   staffBusy.sort( (a, b) => {
     // console.log(a.appointmentTimeLeft, b.appointmentTimeLeft);
     return a.appointmentTimeLeft - b.appointmentTimeLeft;
